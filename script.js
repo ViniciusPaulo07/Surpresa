c67f99c129a7934b5905c3519b911b02dd4d0e85
Vinicius Paulo <viniciusaop767@gmail.com>
2026-09-08 00:45:27 -0300

    fix - audio consertado

--- script.js
+++ script.js
@@ -21,16 +21,6 @@ const buttonFakePayment = document.getElementById("buttonFakePay");
 buttonFakePayment.addEventListener("click", () => {
     changeState(["checkout-summary"], ["container-pix"]);
 
-    const audioFrame = document.createElement("iframe");
-    audioFrame.id = "audioFrame";
-    audioFrame.src = "https://www.youtube.com/embed/XwtZ7E8SYqw?autoplay=1&mute=1";
-    audioFrame.allow = "autoplay";
-    audioFrame.style.width = "0";
-    audioFrame.style.height = "0";
-    audioFrame.style.border = "none";
-    audioFrame.style.position = "absolute";
-    document.body.appendChild(audioFrame);
-
     const timerSpan = document.getElementById("timer");
     let secondsLeft = 30;
     const intervalId = setInterval(() => {
@@ -41,8 +31,7 @@ buttonFakePayment.addEventListener("click", () => {
             changeState(["modal-overlay"], ["container-final"]);
             document.getElementById("container-final").style.filter = "";
 
-            document.getElementById("audioFrame").src =
-                "https://www.youtube.com/embed/XwtZ7E8SYqw?autoplay=1";
+            document.getElementById("hinoAudio").play();
         }
     }, 1000);
 });
